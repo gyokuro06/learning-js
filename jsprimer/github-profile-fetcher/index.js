a7198899c2531c20f701a6c0b2ba682163c02c68
gyokuro06 <gyokuro06@gmail.com>
2023-03-30 00:05:11 +0900

    github-profile-fetcher: add to show avatar icon

--- jsprimer/github-profile-fetcher/index.js
+++ jsprimer/github-profile-fetcher/index.js
@@ -14,13 +14,15 @@ export async function onButtonClick(_) {
     const userId = "gyokuro06";
     const userProfile = await getUserProfile(userId);
     window.document.getElementsByClassName('profile-container').item(0).innerHTML = `
-        <p class="user-name">ユーザー名: ${userProfile.userName}</p>
+        <h4 class="user-name">ユーザー名: ${userProfile.userName}</h4>
+        <img class="avatar-icon" src="${userProfile.avatarIcon}" alt="${userProfile.userName}" height=100>
     `
 }
 
 const toUserProfile = (profile) => {
     return {
-        userName: profile.login
+        userName: profile.login,
+        avatarIcon: profile.avatar_url
     }
 }
 
